3e2e5d17b70625b3954672a8de52e4c69db95b3a
JeongWon-CHO <ahin0312@naver.com>
2026-08-07 13:47:26 +0900

    feat: 안드로이드 위젯 작업 핸들러 등록 코드 추가 및 불필요한 코드 제거

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,1 @@
 import 'expo-router/entry';
-
-import { registerWidgetTaskHandler } from 'react-native-android-widget';
-
-import { widgetTaskHandler } from './widget-task-handler';
-
-registerWidgetTaskHandler(widgetTaskHandler);
